d9def175df4ce2233ddfe32c8074deea1cf97175
Don2x <xldon2lx@gmail.com>
2012-06-05 06:58:49 +0000

    Implemented Scary & Minmax governors

--- kernel/include/linux/cpufreq.h
+++ kernel/include/linux/cpufreq.h
@@ -374,7 +374,10 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
 extern struct cpufreq_governor cpufreq_gov_smartass;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCARY)
+extern struct cpufreq_governor cpufreq_gov_scary;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_scary)
 #endif
 
 
